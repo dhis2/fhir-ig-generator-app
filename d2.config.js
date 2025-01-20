c438d16f99f6d73e77e6b7573e667ed2dea88716
JohanGHole <johan.hole@dhis2.org>
2025-01-20 11:39:34 +0100

    chore: prepare App Hub CD

--- d2.config.js
+++ d2.config.js
@@ -1,9 +1,11 @@
 const config = {
+    id: '1928228f-edbe-461d-b805-8ed94aa582a3',
     type: 'app',
     title: "FHIR IG Generator",
     entryPoints: {
         app: './src/App.js',
     },
+    minDHIS2Version: '2.39'
 }
 
 module.exports = config
