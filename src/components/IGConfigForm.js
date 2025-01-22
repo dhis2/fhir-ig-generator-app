8ed80da5c963ee8e5b6b40cae82fd5a409045a7b
JohanGHole <johan.hole@dhis2.org>
2025-01-22 12:34:12 +0100

    fix: IG config form improvements

--- src/components/IGConfigForm.js
+++ src/components/IGConfigForm.js
@@ -4,128 +4,123 @@ import styles from "./IGConfigForm.module.css";
 
 const IGConfigForm = ({ onSubmit }) => {
   return (
-    <div className={styles.container}>
-      <h2>Implementation Guide Configuration</h2>
-      <ReactFinalForm.Form
-        onSubmit={onSubmit}
-        initialValues={{
-          id: "fhir.example",
-          name: "Example IG",
-          canonical: "http://example.org",
-          status: "draft",
-          version: "0.1.0",
-          releaseLabel: "ci-build",
-          publisher: {
-            name: "DHIS2",
-            url: "https://dhis2.org",
-          },
-        }}
-      >
-        {({ handleSubmit }) => (
-          <form onSubmit={handleSubmit}>
-            <div className={styles.row}>
-              <ReactFinalForm.Field
-                required
-                name="id"
-                label="ID"
-                component={InputFieldFF}
-                className={styles.inputField}
-                validate={hasValue}
-                helpText="The unique identifier for the IG. Default: fhir.example"
-              />
-            </div>
-            <div className={styles.row}>
-              <ReactFinalForm.Field
-                required
-                name="name"
-                label="Name"
-                component={InputFieldFF}
-                className={styles.inputField}
-                validate={hasValue}
-                helpText="Default: ExampleIG"
-              />
-            </div>
-            <div className={styles.row}>
-              <ReactFinalForm.Field
-                required
-                name="canonical"
-                label="Canonical"
-                component={InputFieldFF}
-                className={styles.inputField}
-                validate={hasValue}
-                helpText="Default: http://example.org"
-              />
-            </div>
-            <div className={styles.row}>
-              <ReactFinalForm.Field
-                required
-                name="status"
-                label="Status"
-                component={SingleSelectFieldFF}
-                className={styles.inputField}
-                validate={hasValue}
-                helpText="Default: draft"
-                options={[
-                                { label: 'draft', value: 'draft' },
-                                { label: 'active', value: 'active' },
-                                { label: 'retired', value: 'retired' },
-                                { label: 'unknown', value: 'unknown' },
-                            ]}
-              />
-            </div>
-            <div className={styles.row}>
-              <ReactFinalForm.Field
-                required
-                name="version"
-                label="Version"
-                component={InputFieldFF}
-                className={styles.inputField}
-                validate={hasValue}
-                helpText="Default: 0.1.0"
-              />
-            </div>
-            <div className={styles.row}>
-              <ReactFinalForm.Field
-                required
-                name="releaseLabel"
-                label="Release Label"
-                component={InputFieldFF}
-                className={styles.inputField}
-                validate={hasValue}
-                helpText="Default: ci-build"
-              />
-            </div>
-            <div className={styles.row}>
-              <ReactFinalForm.Field
-                required
-                name="publisher.name"
-                label="Publisher"
-                component={InputFieldFF}
-                className={styles.inputField}
-                validate={hasValue}
-                helpText="Default: DHIS2"
-              />
-            </div>
-            <div className={styles.row}>
-              <ReactFinalForm.Field
-                required
-                name="publisher.url"
-                label="Publisher URL"
-                component={InputFieldFF}
-                className={styles.inputField}
-                validate={hasValue}
-                helpText="Default: https://dhis2.org"
-              />
-            </div>
+    <div className={styles.centerWrapper}>
+      <div className={styles.container}>
+        <h2 className={styles.title}>Implementation Guide Configuration</h2>
+        <ReactFinalForm.Form
+          onSubmit={onSubmit}
+          initialValues={{
+            id: "fhir.example",
+            name: "Example IG",
+            canonical: "http://example.org",
+            status: "draft",
+            version: "0.1.0",
+            releaseLabel: "ci-build",
+            publisher: {
+              name: "DHIS2",
+              url: "https://dhis2.org",
+            },
+          }}
+        >
+          {({ handleSubmit }) => (
+            <form onSubmit={handleSubmit}>
+              <div className={styles.gridContainer}>
+                <ReactFinalForm.Field
+                  required
+                  name="id"
+                  label="ID"
+                  component={InputFieldFF}
+                  className={styles.inputField}
+                  validate={hasValue}
+                  helpText="The unique identifier for the IG. Default: fhir.example"
+                />
 
-            <div className={styles.row}>
-              <Button type="submit" primary large>
-                Set Configuration
-              </Button>
-            </div>
-          </form>
-        )}
-      </ReactFinalForm.Form>
+                <ReactFinalForm.Field
+                  required
+                  name="name"
+                  label="Name"
+                  component={InputFieldFF}
+                  className={styles.inputField}
+                  validate={hasValue}
+                  helpText="Default: ExampleIG"
+                />
+
+                <ReactFinalForm.Field
+                  required
+                  name="canonical"
+                  label="Canonical"
+                  component={InputFieldFF}
+                  className={styles.inputField}
+                  validate={hasValue}
+                  helpText="Default: http://example.org"
+                />
+
+                <ReactFinalForm.Field
+                  required
+                  name="status"
+                  label="Status"
+                  component={SingleSelectFieldFF}
+                  className={styles.inputField}
+                  validate={hasValue}
+                  helpText="Default: draft"
+                  options={[
+                                  { label: 'draft', value: 'draft' },
+                                  { label: 'active', value: 'active' },
+                                  { label: 'retired', value: 'retired' },
+                                  { label: 'unknown', value: 'unknown' },
+                              ]}
+                />
+
+                <ReactFinalForm.Field
+                  required
+                  name="version"
+                  label="Version"
+                  component={InputFieldFF}
+                  className={styles.inputField}
+                  validate={hasValue}
+                  helpText="Default: 0.1.0"
+                />
+
+                <ReactFinalForm.Field
+                  required
+                  name="releaseLabel"
+                  label="Release Label"
+                  component={InputFieldFF}
+                  className={styles.inputField}
+                  validate={hasValue}
+                  helpText="Default: ci-build"
+                />
+
+                <ReactFinalForm.Field
+                  required
+                  name="publisher.name"
+                  label="Publisher"
+                  component={InputFieldFF}
+                  className={styles.inputField}
+                  validate={hasValue}
+                  helpText="Default: DHIS2"
+                />
+
+                <ReactFinalForm.Field
+                  required
+                  name="publisher.url"
+                  label="Publisher URL"
+                  component={InputFieldFF}
+                  className={styles.inputField}
+                  validate={hasValue}
+                  helpText="Default: https://dhis2.org"
+                />
+              </div>
+
+              <div className={styles.button}>
+                <Button type="submit" primary large>
+                  Set Configuration
+                </Button>
+              </div>
+            </form>
+          )}
+        </ReactFinalForm.Form>
+      </div>
     </div>
   );
 };
