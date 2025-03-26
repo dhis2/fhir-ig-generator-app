e2fa7b45fcc675c8ab12d752bf7bcdfaafc97eb0
JohanGHole <johan.hole@dhis2.org>
2025-03-26 14:38:05 +0100

    chore: update dependencies

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { createHashRouter } from "react-router-dom";
+import { createHashRouter } from "react-router";
 import IGConfigPage from "../pages/IGConfigPage";
 import TrackerProgramSelectorPage from "../pages/TrackerProgramSelectorPage";
-import { Navigate } from "react-router-dom";
+import { Navigate } from "react-router";
 
 const createAppRouter = ({ igConfig, setIgConfig }) => {
     return createHashRouter([
