905c68ec4fafd60ac94deadc3ac7d0c732864b7a
JohanGHole <johan.hole@dhis2.org>
2025-04-22 09:38:24 +0200

    fix: typo

--- src/components/PublishingInstructionsModal.jsx
+++ src/components/PublishingInstructionsModal.jsx
@@ -70,7 +70,7 @@ const PublishingInstructionsModal = ({ onClose }) => {
     );
 };
 
-PublishingInstructionsModal.PropTypes = {
+PublishingInstructionsModal.propTypes = {
     onClose: PropTypes.func.isRequired
 }
 
