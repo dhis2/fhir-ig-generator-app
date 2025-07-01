ffe6f9c1fea41a7bb8c3f2cf6fada5062f549103
JohanGHole <johan.hole@dhis2.org>
2025-07-01 12:10:11 +0200

    chore: remove unused exportbutton component

--- src/components/ExportButton.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Button } from '@dhis2/ui';
-
-const ExportButton = ({ onClick }) => (
-    <Button primary onClick={onClick} disabled={programMetadata.length == 0}>Download FHIR IG</Button>
-);
-
-export default ExportButton;
